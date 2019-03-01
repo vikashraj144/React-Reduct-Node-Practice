4e61bbe57a494560e1e61712eb7482788f8f1cf8
vikash <vikashraj144@gmail.com>
2019-03-01 15:42:14 +0530

    practice com added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,6 @@
 import React,{Component} from 'react';
 
 class Home extends Component {
-    constructor(props){
-        super(props);
-    }
-
-    // load all data in store
-    componentDidMount = () =>{
-        // this.props.loadAllData({id:12,name:'vikask'});
-    }
 
     render(){
         return(
---
+++ src/components/Practice.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Practice extends React.Component{
+
+    render(){
+        return(
+            <div>
+                Practice
+            </div>
+        )
+    }
+}
+
+export default Practice;
